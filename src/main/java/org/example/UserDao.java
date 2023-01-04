09523a5e524c3303d466a6d885cbc88c18740818
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-04 18:43:45 +0900

    refactor(JDBCTemplate, PreparedStatementSetter): Delegate pstmt setting

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -7,29 +7,18 @@ import java.sql.SQLException;
 
 public class UserDao {
 	public void create(User user) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		
-		try {
-			con = ConnectionManager.getConnection();
-			String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, user.getUserId());
-			pstmt.setString(2, user.getPassword());
-			pstmt.setString(3, user.getName());
-			pstmt.setString(4, user.getEmail());
-			
-			pstmt.executeUpdate();
-			
-		} finally {
-			if (pstmt != null) {
-				pstmt.close();
+		String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+		jdbcTemplate.executeUpdate(user, sql, new PreparedStatementSetter() {
+			@Override
+			public void setter(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, user.getUserId());
+				pstmt.setString(2, user.getPassword());
+				pstmt.setString(3, user.getName());
+				pstmt.setString(4, user.getEmail());
 			}
-			
-			if (con != null) {
-				con.close();
-			}
-		}
+		});
 	}
 	
 	public User findByUserId(String userId) throws SQLException {
