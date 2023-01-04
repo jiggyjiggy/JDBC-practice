fb07c2da20187d70a500625ba535270b7813a776
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-04 18:51:08 +0900

    refactor: Delegate pstmt setting 2 ; findByUserId

--- src/main/java/org/example/JdbcTemplate.java
+++ src/main/java/org/example/JdbcTemplate.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTemplate {
@@ -18,6 +19,44 @@ public class JdbcTemplate {
 			pstmt.executeUpdate();
 			
 		} finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+	
+	public User executeUpdate(String userId, String sql, PreparedStatementSetter pss, RowMapper rowMapper) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = ConnectionManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			
+			pss.setter(pstmt);
+			
+			rs = pstmt.executeQuery();
+			User user = null;
+			
+			if (rs.next()) {
+				user = new User(
+					rs.getString("userId"),
+					rs.getString("password"),
+					rs.getString("name"),
+					rs.getString("email")
+				);
+			}
+			return user;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			
 			if (pstmt != null) {
 				pstmt.close();
 			}
--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -22,40 +22,24 @@ public class UserDao {
 	}
 	
 	public User findByUserId(String userId) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
 		
-		try {
-			con = ConnectionManager.getConnection();
-			String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, userId);
-			
-			rs = pstmt.executeQuery();
-			User user = null;
-			
-			if (rs.next()) {
-				user = new User(
-					rs.getString("userId"),
-					rs.getString("password"),
-					rs.getString("name"),
-					rs.getString("email")
+		return jdbcTemplate.executeUpdate(userId, sql, new PreparedStatementSetter() {
+			@Override
+			public void setter(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, userId);
+			}
+		}, new RowMapper() {
+			@Override
+			public Object map(ResultSet resultSet) throws SQLException {
+				return new User(
+					resultSet.getString("userId"),
+					resultSet.getString("password"),
+					resultSet.getString("name"),
+					resultSet.getString("email")
 				);
 			}
-			return user;
-		} finally {
-			if (rs != null) {
-				rs.close();
-			}
-			
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			
-			if (con != null) {
-				con.close();
-			}
-		}
+		});
 	}
 }
